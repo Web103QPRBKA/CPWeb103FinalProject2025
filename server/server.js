38e63281cbb93a7f8812a48311db7ba87d3d5308
Anaiq <Anaiq@example.com>
2025-11-14 09:47:59 -0600

    small refactor

--- server/server.js
+++ server/server.js
@@ -6,15 +6,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const PORT = process.env.PORT || 3001;
+
 app.get("/", (req, res) => {
   res
     .status(200)
     .send(
-      '<h1 style="text-align: center; margin-top: 50px;">SERVER IS RUNNING ON 3001!</h1>'
+      `<h1 style="text-align: center; margin-top: 50px;">SERVER IS RUNNING ON ${PORT}!</h1>`
     );
 });
-
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
