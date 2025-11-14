149276fc3d399d3488943c123e1faa08640691bf
Anaiq <Anaiq@example.com>
2025-11-14 12:58:53 -0600

    Creates playerData, adds reset.js to create and seed player table, updates package.json

--- server/config/reset.js
+++ server/config/reset.js
@@ -1,5 +1,52 @@
 import { pool } from "./database.js";
-import "./dotenv.js";
-import { fileURLToPath } from "url";
-import path, { dirname } from "path";
-import fs from "fs";
+import dotenv from "dotenv";
+// import "./dotenv.js";
+dotenv.config({ path: "../.env" });
+
+import { playersData } from "../data/player.js";
+
+const createPlayersTable = async () => {
+  const createPlayersTableQuery = `
+  DROP TABLE IF EXISTS player CASCADE;
+
+  CREATE TABLE IF NOT EXISTS player (
+    id SERIAL PRIMARY KEY,
+    playerName VARCHAR(255) NOT NULL,
+    currentGame INT,
+    gameTheme INT
+  );
+`;
+
+  try {
+    const PlayerRes = await pool.query(createPlayersTableQuery);
+    console.log("🎉 Players table created successfully.", PlayerRes);
+  } catch (error) {
+    console.error("⚠️ Error creating Players table:", error);
+  }
+};
+
+const seedPlayersTable = async () => {
+  await createPlayersTable()
+  playersData.forEach((player) => {
+    const insertQuery = {
+      text: 'INSERT INTO player (playerName, currentGame, gameTheme) VALUES($1, $2, $3)'
+    }
+    const values = [
+      player.playerName,
+      player.currentGame,
+      player.gameTheme
+    ]
+
+    pool.query(insertQuery, values, (err, res) => {
+      if (err) {
+        console.log(`⚠️ Error inserting player: ${player.playerName}`, err);
+        return
+      }
+      console.log(`✅ ${player.playerName} added successfully`);
+    })
+
+  })
+}
+
+seedPlayersTable();
+
---
+++ server/data/player.js
@@ -0,0 +1,14 @@
+export const playersData = [
+  {
+    id: 1,
+    playerName: "PuzzleMaster88",
+    currentGame: 2,
+    gameTheme: 3,
+  },
+  {
+    id: 2,
+    playerName: "LogicLynx",
+    currentGame: 1,
+    gameTheme: 2,
+  },
+];
