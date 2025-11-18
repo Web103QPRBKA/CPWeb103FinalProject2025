252d26fc870fead94fb24efec02cb6321787e955
Anaiq <Anaiq@example.com>
2025-11-18 16:45:55 -0600

    continue refactor welcome page and styles

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -6,18 +6,20 @@ const Welcome = () => {
   return (
     <div>
       <div className="row title-grid-container amber">
-        <div className="col s2">
+        <div className="col s4">
           <Grid />
         </div>
         {/* <div className="game-title-container"> */}
-        <div className="col s7">
-          <h1>Logic puzzle</h1>
+        <div className="col s8">
+          <h1 id="logic-puzzle-title" className="title-container">
+            Logic Puzzle
+          </h1>
         </div>
       </div>
 
       <div className="row">
         <div className="col s12">
-          <h2>Welcome, thinker!</h2>
+          <h2>Welcome, Thinker!</h2>
           <p>
             Your mission: Unravel the clues, outsmart the puzzles, and rise to
             the top.
