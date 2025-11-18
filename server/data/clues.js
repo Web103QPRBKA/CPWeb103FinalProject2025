21cb4ebf20cc8e606fdd841ef25d1fa76f29504b
Anaiq <Anaiq@example.com>
2025-11-18 12:01:43 -0600

    Adds third game data, clues, hints, and solution. minor prettier changes

--- server/data/clues.js
+++ server/data/clues.js
@@ -1,41 +1,63 @@
 export const cluesData = [
   {
-id: 1,
-clue: "Sarah didn't choose fudge ripple or nutty crunch.",
-gameId: 1,
+    id: 1,
+    clue: "Sarah didn't choose fudge ripple or nutty crunch.",
+    gameId: 1,
   },
   {
-id: 2,
-clue: "Lisa had a scoop of cookie dough, but not of vanilla.",
-gameId: 1,
+    id: 2,
+    clue: "Lisa had a scoop of cookie dough, but not of vanilla.",
+    gameId: 1,
   },
   {
-id: 3,
-clue: "No one's cone had both mint madness and vanilla ice cream.",
-gameId: 1,
+    id: 3,
+    clue: "No one's cone had both mint madness and vanilla ice cream.",
+    gameId: 1,
   },
   {
-id: 4,
-clue: `Ozma gave away the magical trumpet at some point before she handed over the object that imparts the power of invisibility,
+    id: 4,
+    clue: `Ozma gave away the magical trumpet at some point before she handed over the object that imparts the power of invisibility,
 which she bestowed upon a friend immediately before giving an item to the lumberjack.`,
-gameId: 2,
+    gameId: 2,
   },
   {
-id: 5,
-clue: `The cowardly cheetah (who didn't receive the object that allows its owner
+    id: 5,
+    clue: `The cowardly cheetah (who didn't receive the object that allows its owner
 to predict the future) was visited two friends before the one who was given the item that gifts 
 its wielder with the ability to see faraway places.`,
-gameId: 2,
+    gameId: 2,
   },
   {
-id: 6,
-clue: `The magical ring wasn't given to the cowardly cheetah; Ozma didn't visit the farmer first.`,
-gameId: 2,
+    id: 6,
+    clue: `The magical ring wasn't given to the cowardly cheetah; Ozma didn't visit the farmer first.`,
+    gameId: 2,
   },
   {
-id: 7,
-clue: `Ozma gave away the magical coronet at some point after she presented the 
+    id: 7,
+    clue: `Ozma gave away the magical coronet at some point after she presented the 
 object that gives its user the power to predict the future (which wasn't given to the good fairy).`,
-gameId: 2,
+    gameId: 2,
+  },
+  {
+    id: 8,
+    clue: `Marvin received $2000 more than the architect who designed a stadium 
+    (who isn't Wilson).`,
+    gameId: 3,
+  },
+  {
+    id: 9,
+    clue: `The architect who designed an opera house received more than the one who
+    made blueprints for a government building (who didn't receive $1000).`,
+    gameId: 3,
+  },
+  {
+    id: 10,
+    clue: `Lance's award was for $4000; neither Charlotte nor Olive received $1000.`,
+    gameId: 3,
+  },
+  {
+    id: 11,
+    clue: `Olive won a grant for her design of a museum.`,
+    gameId: 3,
   },
 ];
--- server/data/solutions.js
+++ server/data/solutions.js
@@ -1,60 +1,96 @@
 export const solutionsData = [
-	{
-		id: 1,
-		solution: [
-			{
-				id: 1,
-				friend: "Lisa",
-				firstScoop: "fudge ripple",
-				secondScoop: "cookie dough",
-			},
-			{
-				id: 2,
-				friend: "Sarah",
-				firstScoop: "chocolate",
-				secondScoop: "mint madness",
-			},
-			{
-				id: 3,
-				friend: "Todd",
-				firstScoop: "vanilla",
-				secondScoop: "nutty crunch",
-			},
-		],
-		gameId: 1,
-	},
-	{
-		id: 2,
-		solution: [
-			{
-				id: 4,
-				friend: "good fairy",
-				visitOrder: "first",
-				object: "trumpet",
-				power: "shift shapes",
-			},
-			{
-				id: 5,
-				friend: "cowardly cheetah",
-				visitOrder: "second",
-				object: "cape",
-				power: "invisibility",
-			},
-			{
-				id: 6,
-				friend: "lumberjack",
-				visitOrder: "third",
-				object: "ring",
-				power: "predict future",
-			},
-			{
-				id: 7,
-				friend: "farmer",
-				visitOrder: "fourth",
-				object: "coronet",
-				power: "see faraway places",
-			},
-		],
-		gameId: 2,
-	},
+  {
+    id: 1,
+    solution: [
+      {
+        id: 1,
+        friend: "Lisa",
+        firstScoop: "fudge ripple",
+        secondScoop: "cookie dough",
+      },
+      {
+        id: 2,
+        friend: "Sarah",
+        firstScoop: "chocolate",
+        secondScoop: "mint madness",
+      },
+      {
+        id: 3,
+        friend: "Todd",
+        firstScoop: "vanilla",
+        secondScoop: "nutty crunch",
+      },
+    ],
+    gameId: 1,
+  },
+  {
+    id: 2,
+    solution: [
+      {
+        id: 4,
+        friend: "good fairy",
+        visitOrder: "first",
+        object: "trumpet",
+        power: "shift shapes",
+      },
+      {
+        id: 5,
+        friend: "cowardly cheetah",
+        visitOrder: "second",
+        object: "cape",
+        power: "invisibility",
+      },
+      {
+        id: 6,
+        friend: "lumberjack",
+        visitOrder: "third",
+        object: "ring",
+        power: "predict future",
+      },
+      {
+        id: 7,
+        friend: "farmer",
+        visitOrder: "fourth",
+        object: "coronet",
+        power: "see faraway places",
+      },
+    ],
+    gameId: 2,
+  },
+  {
+    id: 3,
+    solution: [
+      {
+        id: 8,
+        architect: "Charlotte",
+        project: "stadium",
+        amount: "$3000",
+      },
+      {
+        id: 9,
+        architect: "Lance",
+        project: "government building",
+        amount: "$4000",
+      },
+      {
+        id: 10,
+        architect: "Marvin",
+        project: "opera house",
+        amount: "$5000",
+      },
+      {
+        id: 11,
+        architect: "Olive",
+        project: "museum",
+        amount: "$2000",
+      },
+      {
+        id: 12,
+        architect: "Wilson",
+        project: "condominium complex",
+        amount: "$1000",
+      },
+    ],
+    gameId: 3,
+  },
 ];
